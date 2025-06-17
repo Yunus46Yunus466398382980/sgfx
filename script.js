cd2ff1d4517d0d2eb47c39d18bee2dfefe23e54d
Yunus46Yunus466398382980 <munojat.1585@gmail.com>
2025-06-17 16:10:48 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+let type = prompt('String typidagi malumot kiriting')
+let number = +prompt ('Number typidagi malumot kiriting')
+let boolean = +prompt ('Boolean typidagi malumot kiriting')
+
+let  = confirm("hamma malumotni togri kiritganizga ishonchingiz komilmi?")
+
+alert('Siz kiritgan Akmaljon sozi string typega tegishli' + 'Siz kiritgan 123 sozi nimber typega tegishli' + 'Siz kiritgan true sozi boolean typega tegishli')
+
